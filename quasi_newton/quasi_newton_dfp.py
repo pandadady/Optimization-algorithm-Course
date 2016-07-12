ce8c13ac22aaebd9a1ff91ef4e10e42f5a742740
shiduo <675767685@qq.com>
2016-07-13 00:03:30 +0800

    Update quasi_newton_dfp.py

--- quasi_newton/quasi_newton_dfp.py
+++ quasi_newton/quasi_newton_dfp.py
@@ -2,36 +2,42 @@
 """
 This module is used to learn quasi newton algorithm
 Authors: shiduo(shiduo@baidu.com)
-Date:2016-6-9
+Date:2016-7-13
 """
 
 from numpy import *
 def calc_gradient(weight,dataMat,labelMat,m):
-    hypothesis = sigmoid(sum(weight*dataMat.T))
-    error = labelMat - hypothesis
-    gradient = dataMat*error/m
+    gradient=dot(sigmoid(dot(weight,dataMat.T))-labelMat,dataMat)
     return gradient
-def dfp(dataMat, labelMat, iterNum=5):
+def get_alphA(weight,dataMat,labelMat,dk): 
+    c=float("inf")
+    t=weight
+    for k in range(1,10):
+        a=1.0/k**2
+        weight = t + a * dk
+        f= sum(dot(sigmoid(dot(weight,dataMat.T))-labelMat,dataMat))
+        if abs(f)>c:
+            break
+        c=abs(f)
+        alpha=a
+    return alpha
+def dfp(dataMat, labelMat, iterNum=3):
     dataMat=mat(dataMat)
+    labelMat=mat(labelMat)
     m,n=shape(dataMat)
-    weight0 = zeros((1,n))
+    weight0 = ones((1,n))
     H0=eye(n)
-    while(iterNum):
-        for i in range(m):
-            gradient=calc_gradient(weight0,dataMat[i,],labelMat[i],m)
-            weight1=weight0+gradient*H0
-            S0=weight1-weight0
-            y0=calc_gradient(weight1,dataMat[i,],labelMat[i],m)-calc_gradient(weight0,dataMat[i,],labelMat[i],m)
-            try:
-                M1=S0.T*S0*(S0.T*y0).I
-    #             print H0*y0.T*y0*H0
-    #             print y0.T*y0*H0
-                M2=H0*y0.T*y0*H0*(H0*y0.T*y0).I
-    #             print M2
-            except:
-                continue
-            H1=H0+M1+M2
-            weight0=weight1
-            H0=H1
-        iterNum-=1
+    for j in range(iterNum):
+        gradient=calc_gradient(weight0,dataMat,labelMat,m)
+        dk=-1*dot(gradient,H0)
+        alpha=get_alphA(weight0,dataMat,labelMat,dk)
+        weight1=weight0+alpha*dk
+        S0=weight1-weight0
+        y0=calc_gradient(weight1,dataMat,labelMat,m)-calc_gradient(weight0,dataMat,labelMat,m)
+        M1=S0.T.dot(S0)/(S0.dot(y0.T))
+        M2=H0.dot(y0.T).dot(y0).dot(H0)/(y0.dot(H0).dot(y0.T))* (-1)
+        H1=H0+M1+M2
+        weight0=weight1
+        print weight0
+        H0=H1
     return array(weight1)[0].tolist()
